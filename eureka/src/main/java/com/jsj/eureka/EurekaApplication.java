be994e11d8e81c1b9d03cda66f53b02760f09f8f
jiangshenjie <741294595@qq.com>
2018-10-26 23:57:24 +0800

    1026 jsj 准备进行微服务拆分，增加Zuul限流功能

---
+++ eureka/src/main/java/com/jsj/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.jsj.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
