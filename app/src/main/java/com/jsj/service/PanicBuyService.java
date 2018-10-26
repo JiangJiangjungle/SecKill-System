465267e729a717635e1a085e88595e8776a31cbd
jiangshenjie <741294595@qq.com>
2018-10-27 00:19:28 +0800

    1027 重构重构

---
+++ app/src/main/java/com/jsj/service/PanicBuyService.java
@@ -0,0 +1,60 @@
+package com.jsj.service;
+
+import com.jsj.constant.BuyResultEnum;
+import com.jsj.exception.ServiceException;
+
+public interface PanicBuyService {
+
+    /**
+     * 利用Mysql乐观锁实现抢购
+     *
+     * @param userId
+     * @param productId
+     * @param buyNumber
+     * @return
+     * @throws ServiceException
+     */
+    BuyResultEnum handleByOptimisticLock(String userId, String productId, int buyNumber) throws ServiceException;
+
+    /**
+     * todo 利用Mysql悲观锁实现抢购
+     *
+     * @param userId
+     * @param productId
+     * @param buyNumber
+     * @return
+     * @throws ServiceException
+     */
+    BuyResultEnum handleByPessimisticLock(String userId, String productId, int buyNumber) throws ServiceException;
+
+    /**
+     * 利用redis分布式锁实现抢购
+     *
+     * @param userId
+     * @param productId
+     * @param buyNumber
+     * @return
+     * @throws ServiceException
+     */
+    BuyResultEnum handleByRedisLock(String userId, String productId, int buyNumber) throws ServiceException;
+
+    /**
+     * todo 利用zookeeper分布式锁实现抢购
+     *
+     * @param userId
+     * @param productId
+     * @param buyNumber
+     * @return
+     * @throws ServiceException
+     */
+    BuyResultEnum handleByZookeeperLock(String userId, String productId, int buyNumber) throws ServiceException;
+
+    /**
+     * 查询主数据库的商品库存
+     *
+     * @param productId
+     * @return
+     * @throws ServiceException
+     */
+    int searchStock(String productId) throws ServiceException;
+}
---
+++ app/src/main/java/com/jsj/service/UserService.java
@@ -0,0 +1,28 @@
+package com.jsj.service;
+
+import com.jsj.exception.ServiceException;
+import com.jsj.pojo.entity.UserDO;
+
+/**
+ * @author jiangshenjie
+ * @date 2018-9-15
+ */
+public interface UserService {
+
+    /**
+     * 根据userId查询
+     * @param id
+     * @return
+     * @throws ServiceException
+     */
+    UserDO searchUserById(String id) throws ServiceException;
+
+    /**
+     * 新增用户
+     * @param name
+     * @param phone
+     * @return
+     * @throws ServiceException
+     */
+    boolean addUser(String name, String phone) throws ServiceException;
+}
